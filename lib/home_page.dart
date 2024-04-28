3961ff3fa7fb1a8d258c222e565ad32891a322ad
Fernando Gabriel <fernando.gabriel322@hotmail.com>
2024-04-28 15:54:58 -0300

    chore: icons in post

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,23 +18,33 @@ class _HomePageState extends State<HomePage> {
     'assets/images/mulher2.jpg',
     'assets/images/mulher3.jpg',
     'assets/images/mulher4.jpg',
-    ];
+  ];
+  List<String> posts = [
+    'assets/images/homem1.jpg',
+    'assets/images/homem2.jpg',
+    'assets/images/homem3.jpg',
+    'assets/images/homem4.jpg',
+    'assets/images/mulher1.jpg',
+    'assets/images/mulher2.jpg',
+    'assets/images/mulher3.jpg',
+    'assets/images/mulher4.jpg',
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
-          "assets/images/Instagram-Logo.png", 
+          "assets/images/Instagram-Logo.png",
           height: 65,
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.favorite_border),
-            onPressed: null, 
+            icon: Icon(FontAwesomeIcons.heart),
+            onPressed: null,
           ),
           IconButton(
-            icon: Icon(Icons.message_outlined),
-            onPressed: null, 
+            icon: Icon(FontAwesomeIcons.facebookMessenger),
+            onPressed: null,
           ),
         ],
       ),
@@ -43,29 +54,88 @@ class _HomePageState extends State<HomePage> {
             //Story
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
-              child: Row(children: List.generate(
-                8,
-                (index) => Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 35,
-                        backgroundImage: AssetImage('assets/images/story.png'),
-                        child: CircleAvatar(
-                          radius: 32,
-                          backgroundImage: AssetImage(profileImages[index]),
+              child: Row(
+                children: List.generate(
+                  8,
+                  (index) => Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 35,
+                          backgroundImage:
+                              AssetImage('assets/images/story.png'),
+                          child: CircleAvatar(
+                            radius: 32,
+                            backgroundImage: AssetImage(profileImages[index]),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 10),
-                      Text('Profile Name', style: TextStyle(fontSize: 12, color: Colors.black87))
-                    ],
+                        SizedBox(height: 10),
+                        Text('Profile Name',
+                            style:
+                                TextStyle(fontSize: 12, color: Colors.black87))
+                      ],
+                    ),
                   ),
                 ),
-              ),),
+              ),
             ),
             Divider(),
-            // Stop here
+            Column(
+              children: List.generate(
+                  8,
+                  (index) => Column(
+                        children: [
+                          //Header Post
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: CircleAvatar(
+                                  radius: 14,
+                                  backgroundImage:
+                                      AssetImage('assets/images/story.png'),
+                                  child: CircleAvatar(
+                                    radius: 12,
+                                    backgroundImage:
+                                        AssetImage(profileImages[index]),
+                                  ),
+                                ),
+                              ),
+                              Text('Profile Name'),
+                              Spacer(),
+                              IconButton(
+                                  onPressed: () {}, icon: Icon(Icons.more_vert))
+                            ],
+                          ),
+                          //IMAGE POST
+                          Image.asset(posts[index]),
+                          Divider(),
+                          //FOOTER POST
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(FontAwesomeIcons.heart)
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(FontAwesomeIcons.message)
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(FontAwesomeIcons.paperPlane)
+                              ),
+                              Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.bookmark_border)
+                              ),
+                            ],
+                          )
+                        ],
+                      )),
+            )
           ],
         ),
       ),
